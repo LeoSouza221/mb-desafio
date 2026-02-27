995c1334240c5c7c1abcad6d688d1728cc1d5bdd
LeoSouza221 <leosouza221@gmail.com>
2026-02-27 16:38:47 -0300

    chore: remove unnecessary files and add lint rules

--- mb-frontend/eslint.config.js
+++ mb-frontend/eslint.config.js
@@ -1,30 +1,56 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue, { rules } from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
+import prettier from 'eslint-plugin-prettier'
+import vueParser from 'vue-eslint-parser'
 
 export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{vue,js,mjs,jsx}'],
-  },
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{vue,js,mjs,jsx}']
+	},
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+	globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-  },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser
+			}
+		}
+	},
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+	js.configs.recommended,
+	...pluginVue.configs['flat/essential'],
 
-  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+	...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
-  skipFormatting,
+	skipFormatting,
+
+	{
+		files: ['**/*.vue'],
+		languageOptions: {
+			parser: vueParser
+		}
+	},
+	{
+		plugins: {
+			prettier
+		},
+		rules: {
+			'prettier/prettier': 'error',
+			indent: ['error', 'tab'],
+			'vue/html-indent': ['error', 'tab'],
+			'vue/script-indent': ['error', 'tab', { baseIndent: 1 }],
+			'no-unused-vars': 'warn',
+			'no-console': 'warn',
+			'vue/multi-word-component-names': 'off',
+			'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+			'vue/no-unused-components': 'warn',
+			'vue/require-default-prop': 'off'
+		}
+	}
 ])
---
+++ mb-frontend/src/components/AppButton.vue
@@ -0,0 +1,11 @@
+<template>
+	<button class="app-button">
+		<slot />
+	</button>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style></style>
