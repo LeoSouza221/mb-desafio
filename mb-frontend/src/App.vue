e7c406aa1b4ceb876924cc867324b12ae3286fc1
LeoSouza221 <leosouza221@gmail.com>
2026-02-27 22:11:39 -0300

    feat: create input component with error variant

--- mb-frontend/src/App.vue
+++ mb-frontend/src/App.vue
@@ -7,11 +7,13 @@
 
 	<main>
 		<AppButton variant="primary" :loading="false">Teste button</AppButton>
+		<AppInput label="Teste input" errorMessage="Error message" />
 	</main>
 </template>
 
 <script setup>
 	import AppButton from './components/AppButton.vue'
+	import AppInput from './components/Icon/inputs/AppInput.vue'
 </script>
 
 <style scoped>
